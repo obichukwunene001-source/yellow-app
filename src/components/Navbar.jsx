c33aa061ac9c7b498573101aab331a682f805285
PC <your-email@example.com>
2025-12-31 10:35:26 +0100

    Describe your changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,20 @@ import { Menu, X } from 'lucide-react';
 import React, { useState} from 'react';
 import { NavLink} from 'react-router-dom'
 import { Link} from 'react-router-dom'
+import logo from '../assets/logo.jpg';
 
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
   return (
-  <header className="text-black font-semibold fixed z-2000 w-full top-0 " style={{ background: 'rgb(232,233,220)' }}>
+  <header className="text-black font-semibold fixed z-2000 w-full top-0 bg-[#C8C8C0]" >
     <div className="flex items-center max-w-7xl mx-auto px-4 py-6 justify-between md:px-20">
-        <Link to="/">Logo</Link>
+        <Link to="/" className="flex items-center">
+          <img src={logo} alt="Logo" className="h-12 w-auto" />
+        </Link>
         <nav className="hidden md:flex gap-7 ">
-            <NavLink to="/"className="hover:text-blue-400">Home</NavLink>
+            <NavLink to="/" className="hover:text-blue-400">Home</NavLink>
             <NavLink to="/about"className="hover:text-blue-400">About</NavLink>
             <NavLink to="/services"className="hover:text-blue-400">Services</NavLink>
             <NavLink to="/profile"className="hover:text-blue-400">Account</NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,22 @@ const Home = () => {
 
   return (
     <>
-    <section className="bg-[#E8E9DC] -mt-7 -ml-4">
+    <section className="bg-[#C8C8C0] -mt-13 -ml-4">
       <div className="max-w-7xl mx-auto px-5 lg:px-18 pt-20 py-2 lg:py-30 lg:pt-14  ">
         <div className="lg:grid lg:grid-cols-2 lg:items-center gap-12">
           <div className="text-left">
             <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold leading-tight">Presence that commands attention</h1>
-            <p className="lg:mt-25 mt-6 text-gray-700 max-w-xl">Elevate your event or campaign with authentic star power. From intimate gatherings to global stages, bring something unforgettable to your vision.</p>
+            <p className="lg:mt-25 mt-6 text-gray-900 max-w-xl">Elevate your event or campaign with authentic star power. From intimate gatherings to global stages, bring something unforgettable to your vision.</p>
 
             <div className="lg:mt-10 mt-4 flex items-center">
-              <button className="bg-black text-white font-bold px-6 py-3 rounded-full hover:cursor-pointer">Book now</button>
+              <button className="bg-blue-950/95 text-white font-bold px-6 py-3 rounded-full hover:cursor-pointer">Book now</button>
               <button className="ml-3 bg-white/8 text-black border border-black/20 font-bold px-5 py-2  rounded-full hover:cursor-pointer">Membership</button>
             </div>
           </div>
 
           <div className="mt-10 lg:mt-0 relative flex justify-center">
-            <img src={yellow} alt="Hero Image" className=" w-[250px] h-[340px] lg:w-[380px] lg:h-[520px] object-cover rounded-2xl shadow-sm "/>
-            <img src={tshirt} alt="Accent Image" className="absolute right-5  -top-6 lg:-right-6  md:right-55 w-20 h-20 lg:w-36 lg:h-36 object-cover rounded-2xl shadow-sm border-2 border-yellow-600"/>
+            <img src={yellow} alt="Hero Image" className=" w-[250px] h-[340px] lg:w-[380px] lg:h-[520px] object-cover rounded-2xl shadow-sm border-1 border-gray-500 "/>
+            <img src={tshirt} alt="Accent Image" className="absolute right-5  -top-6 lg:-right-6  md:right-55 w-20 h-20 lg:w-36 lg:h-36 object-cover rounded-2xl shadow-sm border-1 border-blue-950"/>
           </div>
         </div>
       </div>
